57a76b9a7f8c0a2406e184617ee8d1ef1a2290a9
mardorcar <martadorantecardoso@gmail.com>
2020-01-21 18:34:57 +0100

    vuelos compartidos

--- sevilla_vuela/main/forms.py
+++ sevilla_vuela/main/forms.py
@@ -6,7 +6,3 @@ class vuelos_por_destino(forms.Form):
 
 class vuelos_por_origen(forms.Form):
     origen = forms.CharField(label='Origen', widget=forms.TextInput, required=True)
-
-
-class vuelos_por_codigo(forms.Form):
-    codigo = forms.CharField(label='Codigo', widget=forms.TextInput, required=True)
